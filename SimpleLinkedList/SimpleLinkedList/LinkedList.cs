a18ecbe51ac193737aff762211d1b6a72336d8e1
chinki kumari singh <singhchinkikumari@gmail.com>
2022-05-31 12:43:44 +0530

    [chinki]Create a Simple Linked List

---
+++ SimpleLinkedList/SimpleLinkedList/LinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLinkedList
+{
+     class LinkedList
+    {
+            internal Node head;
+            public void Add(int data)
+            {
+                Node node = new Node(data);
+                if (this.head == null)
+                {
+                    this.head = node;
+                }
+                else
+                {
+                    Node temp = head;
+                    while (temp != null)
+                    {
+                        temp = temp.next;
+                    }
+                    temp.next = node;
+                }
+                Console.WriteLine("inserted into the linked list", node.data);
+            }
+            public void Display()
+            {
+                Node temp = this.head;
+                if (temp == null)
+                {
+                    Console.WriteLine("Linked List is empty");
+                }
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+
+     }
+    
+}
---
+++ SimpleLinkedList/SimpleLinkedList/Program.cs
@@ -0,0 +1,5 @@
+﻿SimpleLinkedList.LinkedList customLinkedList = new SimpleLinkedList.CustomLinkedList();
+customLinkedList.Display();
+LinkedList.Add(56);
+LinkedList.Add(30);
+LinkedList.Add(70);
